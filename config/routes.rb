cf67a8f6d07023a0b3a239c5b3fd8c682f8c5ce8
Sujit Pranav <sujitp.r@emamifrankross.com>
2017-05-13 21:07:33 +0530

    Added procfile for deployment & activeadmin bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  ActiveAdmin.routes(self)
+  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
